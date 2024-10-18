2f0bb54c575de6eabb36f1eb539c1062741a0fec
jv291 <jv291@kent.ac.uk>
2024-10-18 09:38:34 +0000

    camel to snake

--- PartOne.py
+++ PartOne.py
@@ -1,1 +1,10 @@
-
+camel = input("input a variable in camel case")
+list(camel)
+snake = " "
+for c in camel:
+    if c.isupper():
+        snake += "_" +c.lower()
+    else:
+        snake += c
+snake
+print (snake)
